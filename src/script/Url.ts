d070ff63a245be5f0c8dd1565638452b38c185fe
melvyn <melpiro>
2023-03-31 15:59:15 +0200

    add missing assets

---
+++ src/script/Url.ts
@@ -0,0 +1,18 @@
+
+
+
+export var helico = require('url:/src/assets/images/helico.gif')
+export var helico_flip = require('url:/src/assets/images/helico_flip.gif')
+export var plane = require('url:/src/assets/images/plane.png')
+export var plane_flip = require('url:/src/assets/images/plane_flip.png')
+export var glider = require('url:/src/assets/images/glider.png')
+export var glider_flip = require('url:/src/assets/images/glider_flip.png')
+export var lightplane = require('url:/src/assets/images/light-plane.png')
+export var lightplane_flip = require('url:/src/assets/images/light-plane_flip.png')
+export var goundvehicle = require('url:/src/assets/images/gound-vehicle.png')
+export var goundvehicle_flip = require('url:/src/assets/images/gound-vehicle_flip.png')
+export var unknown = require('url:/src/assets/images/unknown-plane.png')
+export var unknown_flip = require('url:/src/assets/images/unknown-plane_flip.png')
+
+export var drone = require('url:/src/assets/images/drone.png')
+export var drone_flip = require('url:/src/assets/images/drone.png')
