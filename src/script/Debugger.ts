4342f17983dd91ae8dff61b669867a515dd52b64
melpiro <pirolimel@gmail.com>
2024-10-24 19:25:50 +0200

    add missing files

---
+++ src/script/Debugger.ts
@@ -0,0 +1,37 @@
+import { FlightMap } from "./FlightMap";
+
+
+
+export class Debugger {
+
+    private is_active:boolean = false;
+    private is_open:boolean = false;
+    private map: FlightMap;
+
+    constructor() {
+    }
+
+    public setMap(map: FlightMap) {
+        this.map = map;
+    }
+
+
+    public active(){
+        console.log("active");
+
+        this.is_active = true;
+        this.map.debug_mode_changed();
+    }
+
+    public desactive(){
+        console.log("desactive");
+
+        this.is_active = false;
+        this.is_open = false;
+        this.map.debug_mode_changed();
+    }
+
+    public isActived(){
+        return this.is_active;
+    }
+}
